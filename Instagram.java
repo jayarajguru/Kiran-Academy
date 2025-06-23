efdbc5f15f00dca344526a79195eaa44aafd86fd
Jaya Rajguru <jayarajguru78@gmail.com>
2025-06-23 23:29:34 +0530

    Add files via upload

---
+++ Instagram.java
@@ -0,0 +1,7 @@
+
+public class Instagram {
+	void LoginCheck(String Username,String Password) {
+		System.out.println("Username :"+Username);
+		System.out.println("Password :"+Password);
+	}
+}
---
+++ TestInsta.java
@@ -0,0 +1,7 @@
+
+public class TestInsta {
+	public static void main(String[] args) {
+		Instagram IG=new Instagram();
+		IG.LoginCheck("Jaya","1234");
+	}
+}
